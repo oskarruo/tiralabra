97e7fbb6a8888d68118058522a626a769daf1599
oskarruo <oskar.ruokolainen@gmail.com>
2025-04-18 14:01:11 +0300

    added documentation

--- src/cc.h
+++ src/cc.h
@@ -1,6 +1,14 @@
 #ifndef COMPRESS_H
 #define COMPRESS_H
 
+/**
+ * @brief The main function for the program that calls the
+ * compression/decompression functions.
+ *
+ * @param argc Number of command line arguments.
+ * @param argv Command line arguments.
+ * @return int Exit status of the program.
+ */
 int cc(int argc, char* argv[]);
 
 #endif
